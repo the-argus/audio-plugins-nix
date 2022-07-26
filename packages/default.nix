1aece2b5fe24438975bf0a94217b7d6e60d3f2bb
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-07-26 10:38:40 -0500

    reformatted and added heckscaper set

--- packages/default.nix
+++ packages/default.nix
@@ -1,32 +1,44 @@
 { pkgs, ... }:
+let
+  # HECKSCAPER --------------------------------------------------------------
+  ct0w0 = import ./synths/ct-0w0.nix { inherit pkgs; };
+  ctws1 = import ./synths/ct-ws1.nix { inherit pkgs; };
+  ambi-vac = import ./synths/ambi-vac.nix { inherit pkgs; };
+  damascus = import ./synths/damascus.nix { inherit pkgs; };
+  great-wall = import ./synths/great-wall.nix { inherit pkgs; };
+  ill-logic = import ./synths/ill-logic.nix { inherit pkgs; };
+  ktso = import ./synths/ktso.nix { inherit pkgs; };
+  nuxx = import ./synths/nuxx.nix { inherit pkgs; };
+  ouch = import ./synths/ouch.nix { inherit pkgs; };
+  psykic = import ./synths/psykic.nix { inherit pkgs; };
+  real-animal = import ./synths/real-animal.nix { inherit pkgs; };
+  sota = import ./synths/sota.nix { inherit pkgs; };
+  virtua-dub = import ./synths/virtua-dub.nix { inherit pkgs; };
+
+  ambifx = import ./synths/ambifx.nix { inherit pkgs; };
+  fuzz-buddies = import ./synths/fuzz-buddies.nix { inherit pkgs; };
+  meat-zone = import ./synths/meat-zone.nix { inherit pkgs; };
+in
 {
   synths = {
-    # HECKSCAPER --------------------------------------------------------------
-    ct0w0 = import ./synths/ct-0w0.nix { inherit pkgs; };
-    ctws1 = import ./synths/ct-ws1.nix { inherit pkgs; };
-    ambi-vac = import ./synths/ambi-vac.nix { inherit pkgs; };
-    damascus = import ./synths/damascus.nix { inherit pkgs; };
-    great-wall = import ./synths/great-wall.nix { inherit pkgs; };
-    ill-logic = import ./synths/ill-logic.nix { inherit pkgs; };
-    ktso = import ./synths/ktso.nix { inherit pkgs; };
-    nuxx = import ./synths/nuxx.nix { inherit pkgs; };
-    ouch = import ./synths/ouch.nix { inherit pkgs; };
-    psykic = import ./synths/psykic.nix { inherit pkgs; };
-    real-animal = import ./synths/real-animal.nix { inherit pkgs; };
-    sota = import ./synths/sota.nix { inherit pkgs; };
-    virtua-dub = import ./synths/virtua-dub.nix { inherit pkgs; };
+    # HECKSCAPER
+    inherit ct0w0 ctws1 ambi-vac damascus great-wall ill-logic ktso nuxx ouch psykic real-animal sota virtua-dub;
   };
 
   effects = {
-    # HECKSCAPER --------------------------------------------------------------
-    ambifx = import ./synths/ambifx.nix { inherit pkgs; };
-    fuzz-buddies = import ./synths/fuzz-buddies.nix { inherit pkgs; };
-    meat-zone = import ./synths/meat-zone.nix { inherit pkgs; };
+    # HECKSCAPER
+    inherit ambifx fuzz-buddies meat-zone;
   };
 
   native = {
     synths = {
       dexed = import ./native/synths/dexed.nix { inherit pkgs; };
     };
+  };
+
+  # sets meant to be concatenated with your programs.yabridge.paths
+  sets = {
+    # all heckscaper plugins
+    heckscaper = [ ct0w0 ctws1 ambi-vac damascus great-wall ill-logic ktso nuxx ouch psykic real-animal sota virtua-dub ambifx fuzz-buddies meat-zone ];
   };
 }
