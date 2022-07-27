a7addd1ecf2dcd38da744d7efdc76d7e7556b9cc
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-07-27 09:54:08 -0500

    updated tal verb presets shasum

--- packages/native/effects/TAL-reverb-4.nix
+++ packages/native/effects/TAL-reverb-4.nix
@@ -15,7 +15,7 @@ pkgs.stdenv.mkDerivation {
         name = "TAL-Reverb-4_Vst_Presets";
         src = pkgs.fetchurl {
           url = "https://tal-software.com//downloads/presets/TAL-Reverb-4%20vst3.zip";
-          sha256 = "1l9g4rywcjb7blbwccn5qpd1xpgwxaa98fcypyv4piikljvrbrsr";
+          sha256 = "1x7d73hc5hbbs7z541ra4yvpfp7066ilscgw1jwb6d08xpzcbi6z";
           name = "TAL-Reverb-4_Vst_Presets_src";
         };
         nativeBuildInputs = [ pkgs.unzip ];
