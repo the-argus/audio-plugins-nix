9c7823fe3f09744e3d18e768ab4e5a8acaf570b3
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-07-29 11:57:33 -0500

    patch git clone in fire cmake

--- packages/native/effects/fire.nix
+++ packages/native/effects/fire.nix
@@ -29,9 +29,29 @@ pkgs.stdenv.mkDerivation {
     pcre
   ];
 
-  buildPhase = ''
-    cmake -S . -B ./build
-  '';
+  patchPhase =
+    let
+      juce-src = pkgs.fetchgit {
+        url = "https://github.com/juce-framework/JUCE.git";
+        rev = "4c43bf429e90690cb1f05b7c8a044cc9f5a59e7d";
+        # date = "2022-07-13T22:59:27+01:00";
+        # path = "/nix/store/p3zsd1j0wxbwfbi6q5gs2fsjjyb4357s-JUCE";
+        sha256 = "07ncsgis18gnbpcya85ikgykldnaj5xmyfcrdjm2rzzg30f9n07h";
+        fetchLFS = false;
+        fetchSubmodules = false;
+        deepClone = false;
+        leaveDotGit = false;
+      };
+    in
+    ''
+      mkdir -p ./build/_deps/
+      cp -r ${juce-src} ./build/_deps/juce-src
+    '';
+
+  buildPhase =
+    '' 
+      cmake -S . -B ./build
+    '';
 
   installPhase = ''
     mkdir $out
