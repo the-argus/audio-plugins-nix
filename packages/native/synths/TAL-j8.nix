7e63768b78a108d994c3e985442dd4eaa2ff798f
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-10 23:23:15 -0500

    updated j8

--- packages/native/synths/TAL-j8.nix
+++ packages/native/synths/TAL-j8.nix
@@ -5,7 +5,7 @@ pkgs.stdenv.mkDerivation {
   name = "TAL-J8";
   src = pkgs.fetchurl {
     url = "https://tal-software.com/downloads/plugins/TAL-J-8_64_linux.zip";
-    sha256 = "1xyliqsady3qfvplnjfzqmnh33n4whdiqq8jm7vv2isqv09rm5vm";
+    sha256 = "sha256-BQfDUAUe9Q44PYV8E0XXGYlVgD+4jW2aETgJjgD10lc=";
   };
 
   nativeBuildInputs = [ pkgs.unzip ];
