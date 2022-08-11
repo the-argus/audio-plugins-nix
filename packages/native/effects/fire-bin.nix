b1c312dc9ecb0102fa5785d001743ed03b1f1404
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-10 23:19:27 -0500

    update fire to... previous version?

--- packages/native/effects/fire-bin.nix
+++ packages/native/effects/fire-bin.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }:
 pkgs.stdenv.mkDerivation {
-  name = "Fire-bin";
+  pname = "Fire-bin";
+  version = "0.9.9";
   src = pkgs.fetchurl {
-    url = "https://github.com/jerryuhoo/Fire/releases/download/v1.0.0/Fire-Linux.zip";
-    sha256 = "0hya8sp61p61pzqv5b7pgvf950d5ib0bxc1l718q9cvwl9hwzam8";
+    url = "https://github.com/jerryuhoo/Fire/releases/download/v0.9.9/Fire-Linux.zip";
+    sha256 = "0sx1kx660kzj4kqcrxk0bw3swwv5lblv9kqk7hmpy39pbb8sfirj";
   };
 
   nativeBuildInputs = [ pkgs.unzip ];
