a3adcf030af18fa1b3437516f797edb9bd3c995a
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-12 17:32:01 -0500

    fixed zip fetch

--- packages/effects/TDR-Nova.nix
+++ packages/effects/TDR-Nova.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 pkgs.stdenv.mkDerivation {
   name = "TDR-Nova";
-  src = pkgs.fetchzip {
+  src = pkgs.fetchurl {
     url = "https://www.tokyodawn.net/labs/Nova/2.1.5/TDR%20Nova%20(no%20installer).zip?x24775";
-    name = "TDR-Nova";
+    name = "TDR-Nova.zip";
     sha256 = "11sd79f202r66nlr2s1ym4ln3iyz7c0189i5qvlrzh1lpsj2ma2c";
   };
   
