6582b363ba6eabcb308c3c50070531b733a5d59e
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-09-12 00:23:37 -0400

    update hash again

--- packages/effects/ferric-tds.nix
+++ packages/effects/ferric-tds.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation {
   src = pkgs.fetchurl {
     name = "ferric-tds.zip";
     url = "https://drive.google.com/uc?export=download&id=139uPVuPRQDXLd0qN99QpNKWZJVqlAj4n";
-    sha256 = "sha256-3/Jv7uMigD/jyCgznWu/IyEmJ7cTB2RlU9CoJBwTGmA=";
+    sha256 = "0mrfd7lm9wz4p2rq13m8d2279fdx5s9ai182flfy01ynmbm1w28w";
   };
 
   nativeBuildInputs = [pkgs.unzip];
