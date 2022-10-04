6604c873b749ca1785cadb3c74cf664c25b1a64b
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-10-04 16:20:11 -0400

    update hash for TAL sampler

--- packages/native/synths/TAL-sampler.nix
+++ packages/native/synths/TAL-sampler.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "TAL-Sampler";
   src = pkgs.fetchurl {
     url = "https://tal-software.com/downloads/plugins/TAL-Sampler_64_linux.zip";
-    sha256 = "01xqpjbqnkhk4sjxbrll4zffywgvxpi534779bd49ry2wwqcwyal";
+    sha256 = "sha256-OSj/8mldOLKJRNRNpH5y1tUoqVcHGIQtFcTX6lsktV0=";
   };
 
   passthru.demo = true;
