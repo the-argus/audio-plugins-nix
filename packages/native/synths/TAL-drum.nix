a513b822f5700145e3b4a2d35f4792e16bebfc3b
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-11-17 19:13:15 -0500

    update hash for tal-drum

--- packages/native/synths/TAL-drum.nix
+++ packages/native/synths/TAL-drum.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "TAL-Drum";
   src = pkgs.fetchurl {
     url = "https://tal-software.com/downloads/plugins/TAL-Drum_64_linux.zip";
-    sha256 = "1i54ch5fgarvmdwxpnam8sbncq355hhxllyaad0cjqqhp1m50fya";
+    sha256 = "sha256-KxMHeGpwnhyGk0PigxeLH+5G51HkyghPqw+Wd0FiuL4=";
   };
 
   passthru.demo = true;
