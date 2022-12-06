fd50b8c925650fbf4400e3f447784d4324f1b6b5
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-12-05 19:42:15 -0500

    update baseline 101 hash

--- packages/native/synths/TAL-bassline-101.nix
+++ packages/native/synths/TAL-bassline-101.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "TAL-Bassline-101";
   src = pkgs.fetchurl {
     url = "https://tal-software.com/downloads/plugins/TAL-BassLine-101_64_linux.zip";
-    sha256 = "sha256-QLucUkKPNw3hkWS359pzstYLClzH+hrvS9a/uIoDWyI=";
+    sha256 = "sha256-aJdANYoX+TDsMM4CSJP3Aj2cpBgoiSxaOxS6/XwjLv0=";
   };
 
   passthru = {
