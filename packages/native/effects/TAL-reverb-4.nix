0a45acc023fc80daeec35f19962cf1eaaf5fd465
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-12-05 19:43:30 -0500

    update hash for TAL reverb 4

--- packages/native/effects/TAL-reverb-4.nix
+++ packages/native/effects/TAL-reverb-4.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "TAL-Reverb-4";
   src = pkgs.fetchurl {
     url = "https://tal-software.com/downloads/plugins/TAL-Reverb-4_64_linux.zip";
-    sha256 = "02hgr7i9xghjw5zwh9496hrxdsngwhm0c71qn2raiqdm861iqhr5";
+    sha256 = "sha256-psBDOyquLrLcMUHQzAU1uAdKLsfVD+oiSgqrSUP0F0o=";
   };
 
   nativeBuildInputs = [pkgs.unzip];
