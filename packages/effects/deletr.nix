10c4c94a4178dd8b7e3ecab35ce407119c418b56
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-07-23 12:18:14 -0400

    update deletr

--- packages/effects/deletr.nix
+++ packages/effects/deletr.nix
@@ -2,11 +2,19 @@
 pkgs.stdenv.mkDerivation {
   name = "deletr";
   src = pkgs.fetchurl {
-    url = "https://heckscaper.com/plugins/del/deletr_vst364_2021_11_22.zip";
-    sha256 = "1gd5xh4dqlr8qr1wbxb9765nrra16zfgz39h0m28q12ndv5q3lxh";
+    url = "https://heckscaper.com/ct/e/del_vst364_2021_11_22.zip";
+    sha256 = "1nfj7hhjvci3g4xf7ds5k20syvsl0sn9zyl0ialnkq5mykli93p8";
   };
 
   nativeBuildInputs = [pkgs.unzip];
 
-  installPhase = "cp -r . $out";
+  unpackPhase = ''
+    unzip $src
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r deletr/* $out
+    cp -r del_manual $out
+  '';
 }
