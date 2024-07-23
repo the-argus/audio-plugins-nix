05b8ebd77859e8dd842e0ae45ac28ad50bf03169
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-07-23 11:51:35 -0400

    update TAL-MOD

--- packages/native/synths/TAL-mod.nix
+++ packages/native/synths/TAL-mod.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "TAL-Mod";
   src = pkgs.fetchurl {
     url = "https://tal-software.com/downloads/plugins/TAL-Mod_64_linux.zip";
-    sha256 = "1wmp6xi3s7g8l6rg6c2a556ghbgrb0wcalx157vvbx8vbayjrwsv";
+    sha256 = "sha256-sq0A0Hme8v759Q/4GTMt/dQ5z5fIiYuKYjRz54aRDic=";
   };
 
   passthru.demo = true;
