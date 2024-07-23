c1b986adf4b737e610a0f8ef74820179dff843ef
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-07-23 13:31:42 -0400

    update ktso url

--- packages/synths/ktso.nix
+++ packages/synths/ktso.nix
@@ -2,7 +2,7 @@
 pkgs.stdenv.mkDerivation {
   name = "ktso";
   src = pkgs.fetchurl {
-    url = "https://heckscaper.com/plugins/ktso/ktso_vst364_2021_07_06.zip";
+    url = "https://heckscaper.com/ct/s/ktso_vst364_2021_07_06.zip";
     sha256 = "056bkb94kr0apm1cj5az7wc0p2x6mx7yi477yrz9m8yvw0xwkrsr";
   };
 
