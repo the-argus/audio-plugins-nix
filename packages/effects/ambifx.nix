b2942766d4648a82b33f5495ec31d6f5dcfe1120
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-07-23 11:57:47 -0400

    update ambifx url

--- packages/effects/ambifx.nix
+++ packages/effects/ambifx.nix
@@ -2,7 +2,7 @@
 pkgs.stdenv.mkDerivation {
   name = "Ambifx";
   src = pkgs.fetchurl {
-    url = "https://heckscaper.com/plugins/fx/ambiFX_vst364_2021_05_29.zip";
+    url = "https://heckscaper.com/ct/j/ambifx_vst364_2021_05_29.zip";
     sha256 = "02qx2cmv83ppq8ap0yk8fd8j2mk9yn8yn0da0137nvv1apjvn9nw";
   };
 
