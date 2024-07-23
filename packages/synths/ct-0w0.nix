d7d50ffdb2c9a3cc4e60ca2977e38d9ec4a9422e
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-07-23 12:07:28 -0400

    update ct-0w0

--- packages/synths/ct-0w0.nix
+++ packages/synths/ct-0w0.nix
@@ -2,11 +2,19 @@
 pkgs.stdenv.mkDerivation {
   name = "CT-0W0";
   src = pkgs.fetchurl {
-    url = "https://heckscaper.com/plugins/cb/ct0w0_vst364_20220610.zip";
-    sha256 = "138rj86gbp7dc4iavc880nhvylhy6m6srjpnnbx1i2lknd54islj";
+    url = "https://heckscaper.com/ct/s/ct0w0_vst364_2022_06_10.zip";
+    sha256 = "0misb99fzm6vf1avi59lrvh2czvh02sqi7q2hh244g6qf2a13xf6";
   };
 
   nativeBuildInputs = [pkgs.unzip];
 
-  installPhase = "cp -r . $out";
+  unpackPhase = ''
+    unzip $src
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r CT-0W0/* $out
+    cp -r manual $out
+  '';
 }
