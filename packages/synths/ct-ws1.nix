d6c4795abfde1a5c6b1c002318eaca4fd31291c2
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-07-23 12:08:57 -0400

    update ct-ws1 url

--- packages/synths/ct-ws1.nix
+++ packages/synths/ct-ws1.nix
@@ -2,7 +2,7 @@
 pkgs.stdenv.mkDerivation {
   name = "CT-WS1";
   src = pkgs.fetchurl {
-    url = "https://heckscaper.com/plugins/ws1/ctws1_vst364_20220610.zip";
+    url = "https://heckscaper.com/ct/s/ctws1_vst364_2022_06_10.zip";
     sha256 = "1s0y4vq7n00xclkrm9cahk948j3fvzjx0vdvd7r2xp435pd3yv6x";
   };
 
