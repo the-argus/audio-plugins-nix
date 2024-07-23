4e365fec3a0d76b91ffb0719a11cd14d799fd3d7
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-07-23 17:27:20 -0400

    update sota

--- packages/synths/sota.nix
+++ packages/synths/sota.nix
@@ -2,11 +2,19 @@
 pkgs.stdenv.mkDerivation {
   name = "sota";
   src = pkgs.fetchurl {
-    url = "https://heckscaper.com/plugins/sota/sota_vst364_2021_06_29.zip";
-    sha256 = "094r3c0kixrfxn7f4vqgah814qa5c8y66y1n7vlwwk58yl3r6aid";
+    url = "https://heckscaper.com/ct/s/sota_vst364_2021_06_29.zip";
+    sha256 = "1r4gw8r7g1h779pwg58j3fn6sg0c4ivs2yx1v30r296pg7v3wr7n";
   };
 
   nativeBuildInputs = [pkgs.unzip];
 
-  installPhase = "cp -r . $out";
+  unpackPhase = ''
+    unzip $src
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r sota/* $out
+    cp -r sota_manual $out
+  '';
 }
