7286b5d51ecea3c0316ef5053988bf4e5db01797
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-07-23 11:34:15 -0400

    update TAL DUB X hash

--- packages/native/synths/TAL-dub-x.nix
+++ packages/native/synths/TAL-dub-x.nix
@@ -3,7 +3,7 @@ pkgs.stdenv.mkDerivation {
   name = "TAL-Dub-X";
   src = pkgs.fetchurl {
     url = "https://tal-software.com/downloads/plugins/TAL-Dub-X_64_linux.zip";
-    sha256 = "sha256-jT3GCZGWnJbmlrPRr0/pqDuMSNjyzkXmJGAPfPowpDE=";
+    sha256 = "sha256-HkeSYuFS8ApVqUFZwuA2HUJgi1rQP7y0qeXldDpZ7tw=";
   };
 
   passthru.demo = true;
