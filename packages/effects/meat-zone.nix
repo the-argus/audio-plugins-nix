8a78a930fcf200d8b9e07b0447b9429e12f0b7e1
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-07-23 13:34:09 -0400

    update meat zone

--- packages/effects/meat-zone.nix
+++ packages/effects/meat-zone.nix
@@ -2,11 +2,19 @@
 pkgs.stdenv.mkDerivation {
   name = "meat-zone";
   src = pkgs.fetchurl {
-    url = "https://heckscaper.com/plugins/meat/meatzone_vst364_2022_02_10.zip";
-    sha256 = "00s98yps8sxgpnkmp0ijz5qlrqcf4z2igfmg1di9vmiw2j5k4dz1";
+    url = "https://heckscaper.com/ct/e/meat_vst364_2021_05_30.zip";
+    sha256 = "0a7l6p4azqd5na2pbx37b94hdwvapkzwvm58qy96vci9c2k0frq2";
   };
 
   nativeBuildInputs = [pkgs.unzip];
 
-  installPhase = "cp -r . $out";
+  unpackPhase = ''
+    unzip $src
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r meatzone/* $out
+    cp -r meat_manual $out
+  '';
 }
