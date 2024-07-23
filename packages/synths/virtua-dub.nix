ac13ca8dd46a3ad4c23c9cda713aaa304e2f8384
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-07-23 17:29:19 -0400

    update virtua dub

--- packages/synths/virtua-dub.nix
+++ packages/synths/virtua-dub.nix
@@ -2,7 +2,7 @@
 pkgs.stdenv.mkDerivation {
   name = "virtua-dub";
   src = pkgs.fetchurl {
-    url = "https://heckscaper.com/plugins/dub/virtuadub_vst364_20220906.zip";
+    url = "https://heckscaper.com/ct/s/virtuad_vst364_2022_09_06.zip";
     sha256 = "0mw5xi2zm999ckjnbjc632zcx02fw06qbfzamyhmh44rg6jpmam5";
   };
 
