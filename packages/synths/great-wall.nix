c0d235a496cc77aaebec0ba85b2668cd178024a6
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-07-23 13:26:23 -0400

    update greatwall synth

--- packages/synths/great-wall.nix
+++ packages/synths/great-wall.nix
@@ -2,11 +2,19 @@
 pkgs.stdenv.mkDerivation {
   name = "GreatWall";
   src = pkgs.fetchurl {
-    url = "https://heckscaper.com/plugins/gw/greatwall_vst364_20220610.zip";
-    sha256 = "18qgbyq0d6aclnyldjhfklz54az4b6pmdwkp5xkh98rk6ri2pmr5";
+    url = "https://heckscaper.com/ct/s/great_vst364_2022_06_10.zip";
+    sha256 = "11drsfcv8cb8i9acc9wa5zgsf9vakgz76gpnvhailx1vrfs566d2";
   };
 
   nativeBuildInputs = [pkgs.unzip];
 
-  installPhase = "cp -r . $out";
+  unpackPhase = ''
+    unzip $src
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r greatwall/* $out
+    cp -r manual.png $out
+  '';
 }
