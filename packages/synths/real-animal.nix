bb4e06599238c25944532c032d758550d35c9c52
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-07-23 13:42:11 -0400

    update real-animal

--- packages/synths/real-animal.nix
+++ packages/synths/real-animal.nix
@@ -2,11 +2,19 @@
 pkgs.stdenv.mkDerivation {
   name = "real-animal";
   src = pkgs.fetchurl {
-    url = "https://heckscaper.com/plugins/real/realanimal_vst364_2021_06_01.zip";
-    sha256 = "1l09sdldi5ha3wp4x0pqfag6df0jr231x55m7ird3qm8313pr8gb";
+    url = "https://heckscaper.com/ct/s/real_vst364_2021_05_30.zip";
+    sha256 = "11jm534q48kgjwiqahfxnp66x1ad4n5s86ag0df0mwn78kryb6d6";
   };
 
   nativeBuildInputs = [pkgs.unzip];
 
-  installPhase = "cp -r . $out";
+  unpackPhase = ''
+    unzip $src
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r realanimal/* $out
+    cp -r real_manual $out
+  '';
 }
